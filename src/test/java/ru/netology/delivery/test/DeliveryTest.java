bebd9c7b428f4a022b65ac406369c19a2d842c40
trukhachev495 <sergey.trainings@gmail.com>
2022-08-11 22:58:08 +0300

    настроил проект2

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -6,16 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.delivery.data.DataGenerator;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Configuration.browser;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$x;
-import static ru.netology.delivery.data.DataGenerator.generateDate;
+import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
@@ -25,20 +22,5 @@ class DeliveryTest {
         val firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         val daysToAddForSecondMeeting = 7;
         val secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $x("//input[@placeholder=\"Город\"]").val(validUser.getCity());
-        $x("//input[@type=\"tel\"]").doubleClick().sendKeys("DELETE");
-        $x("//input[@placeholder=\"Дата встречи\"]").val(firstMeetingDate);
-        $("[data-test-id='name'] input").val(validUser.getName());
-        $("[data-test-id='phone'] input").val(validUser.getPhone());
-        $("[data-test-id='agreement']").click();
-        $x("//*[contains(text(),'Запланировать')]").click();
-        $x("//*[contains(text(),'Успешно!')]").should(visible, Duration.ofSeconds(15));
-        $("[data-test-id=success-notification] .notification__content").should(exactText("Встреча успешно запланирована на " + firstMeetingDate));
-        $x("//input[@type=\"tel\"]").doubleClick().sendKeys("DELETE");
-        $x("//input[@placeholder=\"Дата встречи\"]").val(secondMeetingDate);
-        $x("//*[contains(text(),'Запланировать')]").click();
-        $("[data-test-id=replan-notification]").should(visible, Duration.ofSeconds(15));
-        $x("//*[contains(text(),'Перепланировать')]").click();
-        $("[data-test-id=success-notification] .notification__content").should(visible, Duration.ofSeconds(15)).should(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
