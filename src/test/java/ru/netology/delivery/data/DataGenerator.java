1d9853cae9fe158c42b6d220f8b7668113069f5e
trukhachev495 <sergey.trainings@gmail.com>
2022-08-11 22:54:36 +0300

    настроил проект

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,51 @@
+package ru.netology.delivery.data;
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String city = faker.address().city();
+        return city;
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+    }
+
+    public static UserInfo generateUser(String locale) {
+        UserInfo userInfo = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+        return userInfo;
+    }
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
